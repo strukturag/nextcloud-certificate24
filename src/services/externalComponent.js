6532132f9471593db131aa60eae72aa1e2236141
Joachim Bauch <bauch@struktur.de>
2023-02-13 16:44:33 +0100

    Simplify externalComponent.

--- src/services/externalComponent.js
+++ src/services/externalComponent.js
@@ -4,9 +4,7 @@ import getVinegarApi from './vinegarapi.js'
 // eslint-disable-next-line jsdoc/require-jsdoc
 function externalComponent(name) {
 	return defineAsyncComponent(() => {
-		if (window[name]) return window[name]
-
-		window[name] = new Promise((resolve, reject) => {
+		return new Promise((resolve, reject) => {
 			getVinegarApi()
 				.then((api) => {
 					const component = api.components[name]
@@ -15,15 +13,12 @@ function externalComponent(name) {
 						return
 					}
 
-					window[name] = component
 					resolve(component)
 				})
 				.catch((error) => {
 					reject(error)
 				})
 		})
-
-		return window[name]
 	})
 }
 
