467b34c9a8fccda18f551d0f829153807be94f68
Joachim Bauch <bauch@struktur.de>
2023-04-17 10:23:02 +0200

    Add license headers and COPYING file.

--- src/mainLoader.js
+++ src/mainLoader.js
@@ -1,23 +1,23 @@
 /**
- * @copyright Copyright (C) 2022, struktur AG
+ * @copyright Copyright (c) 2022, struktur AG.
  *
- * @author Joachim Bauch <mail@joachim-bauch.de>
+ * @author Joachim Bauch <bauch@struktur.de>
  *
  * @license AGPL-3.0
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
  */
+
 import Vue from 'vue'
 import VueObserveVisibility from 'vue-observe-visibility'
 import { Tooltip } from '@nextcloud/vue'
