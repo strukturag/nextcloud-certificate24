afba301ec870910c797f8c60ffc54c3f2df76188
Joachim Bauch <bauch@struktur.de>
2024-10-09 09:09:48 +0200

    Support running tests locally.

--- tests/php/bootstrap.php
+++ tests/php/bootstrap.php
@@ -22,7 +22,11 @@
 if (!defined('PHPUNIT_RUN')) {
 	define('PHPUNIT_RUN', 1);
 }
-require_once __DIR__ . '/../../../../lib/base.php';
+if (file_exists(__DIR__ . '/../../../nextcloud/lib/base.php')) {
+	require_once __DIR__ . '/../../../nextcloud/lib/base.php';
+} else {
+	require_once __DIR__ . '/../../../../lib/base.php';
+}
 \OC::$loader->addValidRoot(\OC::$SERVERROOT . '/tests');
 \OC_App::loadApp('certificate24');
 if (!class_exists('\PHPUnit\Framework\TestCase')) {
